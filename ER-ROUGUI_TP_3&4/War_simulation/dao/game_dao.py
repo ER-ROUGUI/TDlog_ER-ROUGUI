d926d673d89af7c12311abfee9d23abc6ea41662
ER-ROUGUI <114986162+ER-ROUGUI@users.noreply.github.com>
2022-12-28 14:05:03 +0100

    Update game_dao.py

--- ER-ROUGUI_TP_3&4/War_simulation/dao/game_dao.py
+++ ER-ROUGUI_TP_3&4/War_simulation/dao/game_dao.py
@@ -43,3 +43,79 @@ class VesselEntity(Base):
     hits_to_be_destroyed = Column(Integer)
     type = Column(String)
     battle_field_id = Column(Integer , ForeignKey('Weapon.id'),nullInt = False)
+    def map_to_game(game_entity: GameEntity) -> Game:
+    game = Game(
+        id=game_entity.id,
+        name=game_entity.name,
+        release_date=game_entity.release_date,
+        publisher=game_entity.publisher,
+        platform=game_entity.platform
+    )
+    return game
+
+
+def map_to_game_entity(game: Game) -> GameEntity:
+    game_entity = GameEntity(
+        id=game.id,
+        name=game.name,
+        release_date=game.release_date,
+        publisher=game.publisher,
+        platform=game.platform
+    )
+    return game_entity
+
+def map_to_player_entity(player: Player) -> PlayerEntity:
+    player_entity = PlayerEntity(
+        id=player.id,
+        name=player.name,
+        game_id=player.game.id
+    )
+    return player_entity
+
+def map_to_vessel_entity(vessel: Vessel) -> VesselEntity:
+    vessel_entity = VesselEntity(
+        id=vessel.id,
+        type=vessel.type,
+        battlefield_id=vessel.battlefield.id
+    )
+    return vessel_entity
+
+class GameDao:
+    def __init__(self):
+        Base.metadata.create_all()
+        self.db_session = Session()
+    def create_game(self, game: Game) -> int:
+        game_entity = map_to_game_entity(game)
+        self.db_session.add(game_entity)
+        self.db_session.commit()
+        return game_entity.id
+    def find_game(self, game_id: int) -> Game:
+        stmt = select(GameEntity).where(GameEntity.id == game_id)
+        game_entity = self.db_session.scalars(stmt).one()
+        return map_to_game(game_entity)
+
+
+#cree et mettre PlayerEntity et VesselEntity-----------------------
+
+def create_player(self, player: Player) -> int:
+    player_entity = map_to_player_entity(player)
+    self.db_session.add(player_entity)
+    self.db_session.commit()
+    return player_entity.id
+
+def update_player(self, player: Player) -> None:
+    player_entity = map_to_player_entity(player)
+    self.db_session.merge(player_entity)
+    self.db_session.commit()
+
+def create_vessel(self, vessel: Vessel) -> int:
+    vessel_entity = map_to_vessel_entity(vessel)
+    self.db_session.add(vessel_entity)
+    self.db_session.commit()
+    return vessel_entity.id
+
+def update_vessel(self, vessel: Vessel) -> None:
+    vessel_entity = map_to_vessel_entity(vessel)
+    self.db_session.merge(vessel_entity)
+    self.db_session.commit()
+#----------------------------------------------------------------
